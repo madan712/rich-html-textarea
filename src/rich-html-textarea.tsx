7184cf488348817cb59e3e34a550942bb2a4cc58
madanfd <madan.chaudhary@fanduel.com>
2023-04-23 18:05:50 +0100

    draft

---
+++ src/rich-html-textarea.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface Props {
+  name?: string;
+  placeholder?: string;
+  rows?: number;
+  cols?: number;
+  disabled?: boolean;
+  readonly?: boolean;
+  maxlength?: number;
+  autofocus?: boolean;
+}
+
+const Textarea = (props: Props) => {
+  return <div></div>;
+};
+
+export default Textarea;

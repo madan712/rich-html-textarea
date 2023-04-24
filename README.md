de73869c20c022f5010ca1ad4c61592cf6a3821e
madanfd <madan.chaudhary@fanduel.com>
2023-04-24 11:04:07 +0100

    Updating readme

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# rich-html-textarea
+# A simple react auto suggest input box
+
+This project is a simple example of a rich HTML text area with line numbers and a copy icon. The text area allows users to input and edit code, with line numbers to aid readability, and a copy button for easy sharing.
+
+Please see the [`DEMO`]( https://stackutils.com/#/codeformatter) here
+
+## Install
+
+### npm
+
+```
+npm install --save rich-html-textarea
+```
+
+### yarn
+
+```
+yarn add rich-html-textarea
+```
+
+## Example
+
+```javascript
+import RichTextarea from "rich-html-textarea";
+
+const App = () => {
+  const onChange = (input) => {
+    console.log(input);
+  };
+  
+  return (
+    <div>
+      <RichTextarea onChange={onChange} />
+    </div>
+  );
+};
+
+export default App;
+```
+
+## API
+
+| Prop                            | Type          | Required | Default | Description                                                                                               |
+|:--------------------------------|:--------------|:--------:|:-------:|:----------------------------------------------------------------------------------------------------------|
+| [`onChange`](#onChange)         | Function      |    ✓     |         | This function is used to capture the change in textarea. It can be used to update the state in your file. |
+| [`showLine`](#showLine)         | Boolean       |          |  true   | Show line numbers in the textarea                                                                         |
+| [`showCopyIcon`](#showCopyIcon) | Boolean       |          |  true   | show copy icon on the top right corner of textarea                                                        |
+| [`css`](#css)                   | CSSProperties |          |         | Control width/height and other css properties                                                             |
+| [`name`](#name)                 | String        |          |         | Specifies a name for a text area                                                                          |
+| [`placeholder`](#placeholder)   | String        |          |         | Specifies a short hint that describes the expected value of a text area                                   |
+| [`disabled`](#disabled)         | Boolean       |          |  false  | Specifies that a text area should be disabled                                                             |
+| [`readOnly`](#readOnly)         | Boolean       |          |  false  | Specifies that a text area should be read-only                                                            |
+| [`maxLength`](#maxLength)       | Boolean       |          |  false  | Specifies the maximum number of characters allowed in the text area                                       |
+| [`autoFocus`](#autoFocus)       | Boolean       |          |  false  | Specifies that a text area should automatically get focus when the page loads                             |
+
+
+                                                                                |
--- src/rich-html-textarea.tsx
+++ src/rich-html-textarea.tsx
@@ -1,18 +1,133 @@
-import React from "react";
+import React from 'react';
 
 interface Props {
-  name?: string;
-  placeholder?: string;
-  rows?: number;
-  cols?: number;
-  disabled?: boolean;
-  readonly?: boolean;
-  maxlength?: number;
-  autofocus?: boolean;
+    name?: string;
+    placeholder?: string;
+    disabled?: boolean;
+    readOnly?: boolean;
+    maxLength?: number;
+    autoFocus?: boolean;
+    showLine?: boolean;
+    showCopyIcon?: boolean;
+    css?: React.CSSProperties;
+    onChange: (selected: string) => void;
 }
 
-const Textarea = (props: Props) => {
-  return <div></div>;
+const textareaCommonStyle: React.CSSProperties = {
+    width: '100%',
+    height: '100%',
+    borderColor: '#ccc',
+    lineHeight: '16px',
+    fontSize: '10px',
 };
 
-export default Textarea;
+const textareaLinedStyle: React.CSSProperties = {
+    backgroundImage: "url('http://i.imgur.com/2cOaJ.png')",
+    backgroundAttachment: 'local',
+    backgroundRepeat: 'no-repeat',
+    paddingLeft: '35px',
+    paddingTop: '10px',
+};
+
+const copyButtonStyle: React.CSSProperties = {
+    position: 'absolute',
+    top: '5px',
+    right: '5px',
+    width: '24px',
+    height: '24px',
+    padding: '0',
+    border: 'none',
+    background: 'transparent',
+    cursor: 'pointer',
+};
+
+const CopyIcon = () => {
+    return (
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+            <rect
+                width="24"
+                height="24"
+                stroke="none"
+                fill="#000000"
+                opacity="0"
+            />
+            <g transform="matrix(1 0 0 1 12 12)">
+                <path
+                    style={{
+                        stroke: 'none',
+                        strokeWidth: 1,
+                        strokeDasharray: 'none',
+                        strokeLinecap: 'butt',
+                        strokeDashoffset: 0,
+                        strokeLinejoin: 'miter',
+                        strokeMiterlimit: 4,
+                        fill: 'rgb(0,0,0)',
+                        fillRule: 'nonzero',
+                        opacity: 1,
+                    }}
+                    transform="translate(-12, -12)"
+                    d="M 4 2 C 2.895 2 2 2.895 2 4 L 2 18 L 4 18 L 4 4 L 18 4 L 18 2 L 4 2 z M 8 6 C 6.895 6 6 6.895 6 8 L 6 20 C 6 21.105 6.895 22 8 22 L 20 22 C 21.105 22 22 21.105 22 20 L 22 8 C 22 6.895 21.105 6 20 6 L 8 6 z M 8 8 L 20 8 L 20 20 L 8 20 L 8 8 z"
+                    strokeLinecap="round"
+                />
+            </g>
+        </svg>
+    );
+};
+
+const RichTextarea = (props: Props) => {
+    const {
+        showLine = true,
+        showCopyIcon = true,
+        disabled = false,
+        readOnly = false,
+        autoFocus = false,
+    } = props;
+
+    const onChange = (event: any) => {
+        const input: string = event.target.value;
+        props.onChange(input);
+    };
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+                ...props.css,
+            }}
+        >
+            <textarea
+                style={
+                    showLine
+                        ? { ...textareaLinedStyle, ...textareaCommonStyle }
+                        : { ...textareaCommonStyle }
+                }
+                placeholder={props.placeholder}
+                name={props.name}
+                disabled={disabled}
+                readOnly={readOnly}
+                maxLength={props.maxLength}
+                autoFocus={autoFocus}
+                onChange={onChange}
+            />
+            {showCopyIcon && (
+                <button
+                    style={copyButtonStyle}
+                    onClick={() => navigator.clipboard.writeText('copied')}
+                >
+                    <CopyIcon />
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default RichTextarea;
